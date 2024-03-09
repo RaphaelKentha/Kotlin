17f11859ab08a82840e06090edf3f9ff69150edf
RaphaelKentha <137506515+RaphaelKentha@users.noreply.github.com>
2024-03-08 22:53:20 -0300

    update
    alteração na nomenclatura dos arquivos

---
+++ Primeiro_codigo_kotlin/src/001primeiroCodigoEVariaveis.kt
@@ -0,0 +1,28 @@
+fun main() {
+    print("hello")
+    // Aprendendo sobre variaveis
+    var idade = 24 // var e mutavel
+    val nome = "ken" // val e imutavel
+    println(idade)
+    idade = 25
+    println(idade)
+    // inteiros:
+    // val idade2 = 22; val idade3: Int = 23
+
+    // float
+    // val altura = 1.2f; val altura2: Float = 2.2f
+
+    // double > float e maior que o float, por isso exibe mais caracteres
+    // val PI: Double = 3.141592
+
+    // char: guarda so 1 caractere
+    // val iniciais: Char = 'K'
+
+    // stirings - fio: guarda mais caracteres
+    // val aplelido: String = "Crow"
+
+    // booleans
+    // val vivo = false
+    // val morto: Boolean = false
+    // val maiorque = 2 > 1
+}
